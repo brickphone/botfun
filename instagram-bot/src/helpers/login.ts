bf6eb98c4494a72cd7e90c455d72195e2ab7760b
brickphone <93726614+brickphone@users.noreply.github.com>
2024-05-28 20:29:27 +0200

    Re-organized

---
+++ instagram-bot/src/helpers/login.ts
@@ -0,0 +1,115 @@
+import { Page } from "puppeteer";
+
+const username = 'xcoinsfifa';
+const password = 'Hurrmas123';
+
+const delay = (time: number) => new Promise(resolve => setTimeout(resolve, time));
+
+const acceptCookies = async (page: Page) => {
+    // Wait for the cookies popup and click "Accept All" if it appears
+    try {
+      await page.waitForSelector('button', { visible: true, timeout: 10000 });
+      
+      // Use page.evaluate to handle the cookies popup
+      const accepted = await page.evaluate(() => {
+        const buttons = Array.from(document.querySelectorAll('button'));
+        const acceptButton = buttons.find(button => button.textContent?.includes('Allow all cookies') || button.textContent?.includes('Allow'));
+        if (acceptButton) {
+          acceptButton.click();
+          return true;
+        }
+        return false;
+      });
+  
+      if (accepted) {
+        console.log('Accepted cookies.');
+      } else {
+        console.log('Cookies popup not found.');
+      }
+    } catch (error) {
+      console.log('No cookies popup found or error occurred:', error);
+    }
+  };
+
+export const login = async (page: Page) => {
+  console.log("Navigating to Instagram login page...");
+  await page.goto('https://www.instagram.com/accounts/login/', { waitUntil: 'networkidle0' });
+  console.log("Page fully loaded.");
+
+  await acceptCookies(page);
+
+  await page.waitForSelector('input[name="username"]');
+  await page.type('input[name="username"]', username);
+  await page.type('input[name="password"]', password);
+
+  // Click login button
+  const loginClicked = await page.evaluate(() => {
+    const buttons = Array.from(document.querySelectorAll('button'));
+    const loginButton = buttons.find(button => button.textContent?.includes('Log in') || button.textContent?.includes('Login'));
+    if (loginButton) {
+      (loginButton as HTMLElement).click();
+      return true;
+    }
+    return false;
+  });
+
+  if (loginClicked) {
+    console.log('Clicked login button.');
+  } else {
+    console.log('Login button not found.');
+  }
+
+  // Wait for navigation to complete the login process
+  try {
+    await page.waitForNavigation({ waitUntil: 'networkidle0' });
+    console.log('Logged in successfully.');
+  } catch (error) {
+    console.error('Error waiting for navigation after login:', error);
+  }
+
+  // Wait and handle "save login info" popup
+  try {
+    console.log('Waiting for "save login info" popup...');
+    await delay(5000); // Adding a delay to ensure the popup has time to appear
+    const loginInfoPopupClicked = await page.evaluate(() => {
+      const divs = Array.from(document.querySelectorAll('div[role="button"]'));
+      const loginPopupButton = divs.find(div => div.textContent?.includes('Not now'));
+      if (loginPopupButton) {
+        (loginPopupButton as HTMLElement).click();
+        return true;
+      }
+      return false;
+    });
+
+    if (loginInfoPopupClicked) {
+      console.log("Clicked 'Not now' on login info popup.");
+    } else {
+      console.log('Login info popup not found.');
+    }
+  } catch (error) {
+    console.log('No login info popup found or error occurred:', error);
+  }
+
+  // Wait and handle "notifications popup"
+  try {
+    console.log('Waiting for "notifications" popup...');
+    await delay(5000);
+    const notificationsPopupClicked = await page.evaluate(() => {
+      const buttons = Array.from(document.querySelectorAll('button'));
+      const notifyButton = buttons.find(button => button.textContent?.includes('Not Now'));
+      if (notifyButton) {
+        (notifyButton as HTMLElement).click();
+        return true;
+      }
+      return false;
+    });
+
+    if (notificationsPopupClicked) {
+      console.log("Clicked 'Not Now' on notifications popup.");
+    } else {
+      console.log('Notifications popup not found.');
+    }
+  } catch (error) {
+    console.log('No notifications popup found or error occurred:', error);
+  }
+};
---
+++ instagram-bot/src/index.ts
@@ -0,0 +1,23 @@
+import puppeteer, { Page } from "puppeteer";
+import { login } from "./helpers/login";
+
+const initializeBot = async () => {
+  const browser = await puppeteer.launch({
+    headless: false,
+    executablePath: puppeteer.executablePath(),
+  });
+  const page = await browser.newPage();
+
+  // Login in
+  await login(page);
+
+  const postUrl = 'https://www.instagram.com/p/C7Y-_7BtQs5';
+  await page.goto(postUrl, { waitUntil: 'networkidle0' });
+  console.log("Post loaded");
+
+  await browser.close();
+};
+
+initializeBot().catch((error) => {
+  console.error('An error occurred:', error);
+});
